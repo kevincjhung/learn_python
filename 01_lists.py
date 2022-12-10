fd57919e87cd0345dbb4e39e7f5a37ccac4bfac9
Kevin Hung <kevincjhung@gmail.com>
2022-12-10 12:21:09 -0800

    Create 01_lists.py

---
+++ 01_lists.py
@@ -0,0 +1,11 @@
+# lists are ordered, mutable and allowed for duplicate values
+
+mylist = ['dog', 'cat', 'mouse', 'dog']
+print(mylist)
+
+# can also do it this way
+list2 = list()
+list2.append('dog') # append this way
+
+# lists can have different data types
+
